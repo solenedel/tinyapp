96257bf8098f70cd38c0668a9c50d2b7cf1312cd
Solene D <solene98@student.ubc.ca>
2021-08-02 22:30:57 +0000

    created generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,16 +1,24 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const PORT = 3001; //default port 3001
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 const urlDatabase = { 
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com", 
   "S152tx": "http://www.example.org", 
 };
+
+// FUNCTION: generateRandomString 6 alphanumeric numbers
+const generateRandomString = () => {
+  return Math.random().toString(36).substr(2, 6);
+};
+
 
 
 app.listen(PORT, () => {
@@ -31,6 +39,11 @@ app.get('/urls/:shortURL', (request, response) => {
 app.get('/urls', (request, response) => {
   const templateVars = { urls: urlDatabase };
   response.render('urls_index', templateVars);
+});
+
+app.post('/urls', (request, response) => {
+  console.log(request.body);
+  response.send('OK');
 });
 
 // handles the root path
