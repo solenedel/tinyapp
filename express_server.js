163de5c2e8dadbacc0725ea8dd92fb3056084c05
Solene D <solene98@student.ubc.ca>
2021-08-02 21:00:02 +0000

    fixes order of path get requests in express_server.js

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
@@ -5,9 +6,10 @@ const PORT = 3001; //default port 3001
 
 app.set('view engine', 'ejs');
 
-const urlDatabase = {
+const urlDatabase = { 
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com", 
+  "S152tx": "http://www.example.org", 
 };
 
 
@@ -15,36 +17,39 @@ app.listen(PORT, () => {
   console.log(`Example app is listening on port ${PORT}`);
 });
 
-// handles the root path
-app.get('/', (request, response) => {
-  response.send('Hello!');
-});
 
-// getting the urls in JSON format
-app.get('/urls.json', (request, response) => {
-  response.json(urlDatabase);
-});
-
-// HTML in the response
-app.get('/hello', (request, response) => {
-  response.send('<html><body>Hello <b>World</b></body></html>\n')
-});
-
+app.get('/urls/:shortURL', (request, response) => {  
+  const templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL] };
+  response.render('urls_show', templateVars);
+}); 
 
 app.get('/urls', (request, response) => {
   const templateVars = { urls: urlDatabase };
   response.render('urls_index', templateVars);
 });
 
-app.get('/urls_show', (request, response) => {
-  const templateVars = { shortURL: urlDatabase, longURL: urlDatabase };
-  response.render('urls_show', templateVars);
+// handles the root path
+app.get('/', (request, response) => {
+  response.send('Hello!');
 });
 
-app.get('/urls/:shortURL', (request, response) => {
-  const templateVars = { shortURL: request.params.shortURL, longURL: request.params.longURL };
-  response.render('urls_show', templateVars);
-});
+// app.get('/urls_show', (request, response) => {
+//   const templateVars = { shortURL: urlDatabase, longURL: urlDatabase };
+//   response.render('urls_show', templateVars);
+// });
+
+
+// // HTML in the response
+// app.get('/hello', (request, response) => {
+//   response.send('<html><body>Hello <b>World</b></body></html>\n')
+// });
+
+// getting the urls in JSON format
+// app.get('/urls.json', (request, response) => {
+//   response.json(urlDatabase);
+// });
+
+
 
 
 
