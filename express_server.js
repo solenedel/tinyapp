fcd4821a5d72cece9a878bd4848f17cf627bec3f
Solene D <solene98@student.ubc.ca>
2021-08-06 07:57:15 +0000

    cleaned up express_server.js - removed useless comments, added some comments

--- express_server.js
+++ express_server.js
@@ -1,11 +1,3 @@
-// ------------------ NOTES & TO-DO LIST ------------------------ //
-
-// morgan
-// cannot set headers after they are set
-// reorder get and post requests to separate them
-// GET routes
-// error pages 404, 403, 400
-
 
 // ----------------- DEPENDENCIES & SETUP ------------------------- //
 
@@ -68,21 +60,19 @@ const users = {
 };
 
 
-// ------------------ EXPORTED -------------------- //
+// ------------------ DATA EXPORTS -------------------- //
 
 module.exports = { urlDatabase, users };
 
+// ------------------- GET ROUTES -------------------------- //
 
-
-// ------------------- ENDPOINTS & ROUTES -------------------------- //
-
-// GET request: render urls_new.ejs HTML template for the respective path
+// GET: new URL creation page
 app.get('/urls/new', (request, response) => {
   
   // if user is not logged in: redirect to login
   if (!request.session.user_id) {
     response.send("you must log in to create a URL.")
-    //response.redirect('/login');
+    //response.redirect('/login'); ⚠️
   }
   
   const templateVars = {
@@ -93,7 +83,7 @@ app.get('/urls/new', (request, response) => {
 });
 
 
-
+// GET: registration page
 app.get('/register', (request, response) => {
   
   const templateVars = {
@@ -104,8 +94,19 @@ app.get('/register', (request, response) => {
 });
 
 
+// GET: login
+app.get('/login', (request, response) => {
+  
+  const templateVars = {
+    user: users[request.session.user_id]
+  };
 
-// POST request: user login
+  response.render('login_form', templateVars);
+
+});
+
+
+// POST: login page
 app.post('/login', (request, response) => {
   
   // login details entered by user denoted as 'test-'
@@ -125,63 +126,6 @@ app.post('/login', (request, response) => {
   }
   
 });
-
-
-
-// GET request: user login
-app.get('/login', (request, response) => {
-  
-  const templateVars = {
-    user: users[request.session.user_id]
-  };
-
-  response.render('login_form', templateVars);
-
-});
-
-
-
-// POST: user registration
-app.post('/register', (request, response) => {
-
-  // check for blank email/passwords entered by user
-  if (request.body.email === '' || request.body.password === '') {
-    response.status(400).send("email or password not valid. Please try again");
-  }
-  
-  const testEmail = request.body.email;
-  const password = request.body.password;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-
-
-  if (emailLookup(testEmail, users)) {
-    response.status(400).send("This email is already registered in our system");
-    return;
-  };
-
-  // generate random user ID
-  const userID = generateRandomString();
-
-  request.session.user_id = userID; ////?
-  request.session.email = testEmail;
-  request.session.password = hashedPassword;
-  
-
-  // create new user in users object
-  users[userID]  = {
-    id: userID,
-    email: testEmail,
-    password: hashedPassword
-  };
-
-  response.redirect('/urls');
-});
-
-// GET: root path- redirect to login page
-app.get('/', (request, response) => {
-  response.redirect('/login');
-});
-
 
 
 // GET: "my URLs" page
@@ -205,9 +149,6 @@ app.get('/urls', (request, response) => {
 });
 
 
-
-// 
-
 app.get('/urls/:shortURL', (request, response) => {
 
   const templateVars = { shortURL: request.params.shortURL,
@@ -217,7 +158,6 @@ app.get('/urls/:shortURL', (request, response) => {
 
   response.render('urls_show', templateVars);
 });
-
 
 
 // GET: redirection of the shortURL into the longURL (ex. S152tx --> www.example.org )
@@ -249,8 +189,53 @@ app.get('/u/:shortURL', (request, response) => {
 });
 
 
+// GET: root path- redirect to login page
+app.get('/', (request, response) => {
+  response.redirect('/login');
+});
 
-// POST: logout
+// ------------------ POST ROUTES ------------------- //
+
+
+// POST: user registration page
+app.post('/register', (request, response) => {
+
+  // check for blank email/passwords entered by user
+  if (request.body.email === '' || request.body.password === '') {
+    response.status(400).send("email or password not valid. Please try again");
+  }
+  
+  // retrieve credentials entered by user and hash the password.
+  const testEmail = request.body.email;
+  const password = request.body.password;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  if (emailLookup(testEmail, users)) {
+    response.status(400).send("This email is already registered in our system");
+    return;
+  };
+
+  // generate random user ID
+  const userID = generateRandomString();
+
+  // create session cookies
+  request.session.user_id = userID; 
+  request.session.email = testEmail;
+  request.session.password = hashedPassword;
+  
+  // create new user in users object
+  users[userID]  = {
+    id: userID,
+    email: testEmail,
+    password: hashedPassword
+  };
+
+  response.redirect('/urls');
+});
+
+
+
+// POST: logout page
 app.post('/logout', (request, response) => {
 
   // clear all session cookies
@@ -262,36 +247,27 @@ app.post('/logout', (request, response) => {
 });
 
 
-
-
-
-
-
-// POST: user changes associated longURL
+// POST: user changing longURL associated with existing shortURL
 app.post('/urls/:shortURL', (request, response) => {
 
-// if linking to someone elses site, make sure there is http or https!!!!!
 
 
 const longURL = request.body.longURL;
 const shortURL = request.params.shortURL;
 
-// NEED TO DRY UP THIS CODE!!
-// add http(://) to the longURL if user did not include it
+// ⚠️ NOTE ⚠️ I tried to call my appendHttp helper function for the task below,
+// th DRY up the code. But ran into many bugs for this case and could not figure it out in time. 
+
+// add 'http://' to the longURL if user did not include it
 if (!(request.body.update).includes('http')) {
     request.body.update = 'http://' + request.body.update;
   }
 
+  // update longURL in urlDatabase
   urlDatabase[shortURL]['longURL'] = request.body.update;
-  
 
   response.redirect(303, `/urls/${request.params.shortURL}`);
-
-
-  //response.redirect('/urls');
 });
-
-
 
 
 // POST: when user clicks on delete button
@@ -302,7 +278,6 @@ app.post('/urls/:shortURL/delete', (request, response) => {
 
   response.redirect('/urls');
 });
-
 
 
 // POST: post newly generated shortURL to the /urls page
@@ -316,19 +291,14 @@ app.post('/urls', (request, response) => {
   // set shortURL equal to function return value
   const shortURL = generateRandomString();
 
+  const longURL = request.body.longURL;
 
- 
-const longURL = request.body.longURL;
-
-// append http to the longURL if required
-
-const finalURL = (appendHttp(request.body.longURL));
-//console.log('new longURL:', finalURL);
-
-
+  // append http:// to the longURL if required
+  const finalURL = (appendHttp(request.body.longURL));
+  
   // set the value of the new unique shortURL key to the longURL
   urlDatabase[shortURL] = {longURL: finalURL,
-    userID: request.session.user_id };
+  userID: request.session.user_id };
   
   // redirect to the respective shortURL page
   response.redirect(`/urls/${shortURL}`);
@@ -340,8 +310,6 @@ const finalURL = (appendHttp(request.body.longURL));
 app.get('/404', (request, response) => {
   response.send("The page you are looking for cannot be found.");
 });
-
-
 
 
 // ---------------- DO NOT EDIT BELOW ---------------------- //
